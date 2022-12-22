528a4d26619eae0064042d15c35baa3edd15cdf7
JaredMathis <jared.mathis@native.consulting>
2022-12-22 06:06:29 -0500

    refactor

--- gen.py
+++ gen.py
@@ -15,6 +15,12 @@ def directory_for_each_if_numeric(parent, for_each):
         if f.isnumeric():
             for_each(f)
 
+def file_html_parse(file_path):
+    with open(file_path, 'r', encoding='utf-8') as source:
+        parsed = html_parse(source)
+        return parsed
+
+
 for version in versions:
     index = {}
     version_directory_input = os.path.join(directory_wordproject, version)
@@ -24,33 +30,32 @@ for version in versions:
         files = os.listdir(book_directory)
         for chapter in files:
             f_path = os.path.join(book_directory, chapter)
-            with open(f_path, 'r', encoding='utf-8') as source:
-                output_directory = os.path.join(directory_public, book_directory)
-                dir_create_if_not_exists(output_directory)
-                output_path = os.path.join(output_directory, str_ending_rename(chapter, '.htm', '') + ".json")
-                with open(output_path, 'w', encoding='utf-8') as output:
-                    parsed = html_parse(source)
-                    h = parsed.find_all('h1')
-                    book = list_first(h).text.strip()
-                    if not book_number in index:
-                        index[book_number] = {}
-                        index[book_number]["name"] = book
-                        index[book_number]["chapters"] = []
-                    s = parsed.find_all('span', {"class":"chapread"})
-                    chapter = list_first(s).text.strip()
-                    index[book_number]["chapters"].append(chapter)
-                    # Make sure the chapters are sorted by integer and not string
-                    index[book_number]["chapters"] = sorted(index[book_number]["chapters"], key=lambda e:int(e))
-                    verses = str(parsed.find_all('p')[-2])
-                    verses_list = verses.split('<br/>')
-                    result = []
-                    for v in verses_list:
-                        t = html_parse(v).text.strip('\n')
-                        s = t.split(' ')
-                        verse = s[0]
-                        tokens = s[1:] 
-                        result.append({"book":book,"chapter":chapter,"verse":verse,"tokens":tokens})
-                    json.dump(result, output, ensure_ascii=False, indent=4)
+            parsed = file_html_parse(f_path)
+            output_directory = os.path.join(directory_public, book_directory)
+            dir_create_if_not_exists(output_directory)
+            output_path = os.path.join(output_directory, str_ending_rename(chapter, '.htm', '') + ".json")
+            with open(output_path, 'w', encoding='utf-8') as output:
+                h = parsed.find_all('h1')
+                book = list_first(h).text.strip()
+                if not book_number in index:
+                    index[book_number] = {}
+                    index[book_number]["name"] = book
+                    index[book_number]["chapters"] = []
+                s = parsed.find_all('span', {"class":"chapread"})
+                chapter = list_first(s).text.strip()
+                index[book_number]["chapters"].append(chapter)
+                # Make sure the chapters are sorted by integer and not string
+                index[book_number]["chapters"] = sorted(index[book_number]["chapters"], key=lambda e:int(e))
+                verses = str(parsed.find_all('p')[-2])
+                verses_list = verses.split('<br/>')
+                result = []
+                for v in verses_list:
+                    t = html_parse(v).text.strip('\n')
+                    s = t.split(' ')
+                    verse = s[0]
+                    tokens = s[1:] 
+                    result.append({"book":book,"chapter":chapter,"verse":verse,"tokens":tokens})
+                json.dump(result, output, ensure_ascii=False, indent=4)
     directory_for_each_if_numeric(version_directory_input, for_each)
     
     output_path = os.path.join(version_directory_output, 'index.json')
