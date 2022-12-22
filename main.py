08c42e1da319d1f8966bb478a0e0e7296388529c
JaredMathis <jared.mathis@native.consulting>
2022-12-21 21:07:54 -0500

    progress

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import random
 from time import sleep
-import urllib.request
 import base64
 import os
-import urllib
+from urllib.request import urlopen
 
 url = "https://www.vatican.va/archive/ESL0506/_INDEX.HTM"
 cached_path = 'cached_websites'
 
-def http_get(url, cached_path):
+def dir_create_if_not_exists(my_path):
+    if os.path.exists(my_path):
+        return
+    else:
+        os.makedirs(my_path)
+
+def http_get_cached(url, cached_path):
     factor = 1000
     sleep_time = random.randrange(5 * factor, 10  * factor) / factor
 
-    encoded = base64.b64encode(url)
+    encoded = base64.b64encode(url.encode()).decode()
     encoded_path = os.path.join(cached_path, encoded + '.htm')
 
+    dir_create_if_not_exists(cached_path)
+
     if not os.path.exists(encoded_path):
+        print(f'Sleeping for {sleep_time}s')
         sleep(sleep_time)
-        content = urllib.urlopen(url)
-        with open(encoded_path, 'w') as f:
-            f.write(content)
+        http_get_save(url, encoded_path)
 
     with open(encoded_path, 'r') as opened:
-        text = opened.read()
+        body = opened.read()
+        return body
 
-http_get(url, cached_path)
+def http_get_save(url, encoded_path):
+    with urlopen(url) as response:
+        body = response.read().decode()
+        with open(encoded_path, 'w') as f:
+            f.write(body)
+
+http_get_cached(url, cached_path)
 
