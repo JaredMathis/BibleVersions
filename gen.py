4e04f84a5d20dd4ff6f640b59c2058c9b860282e
JaredMathis <jared.mathis@native.consulting>
2022-12-22 06:03:26 -0500

    progress

--- gen.py
+++ gen.py
@@ -16,9 +16,11 @@ def directory_for_each_if_numeric(parent, for_each):
             for_each(f)
 
 for version in versions:
-    version_directory = os.path.join(directory_wordproject, version)
-    def for_each(book):
-        book_directory = os.path.join(version_directory, book)
+    index = {}
+    version_directory_input = os.path.join(directory_wordproject, version)
+    version_directory_output = os.path.join(directory_public, version_directory_input)
+    def for_each(book_number):
+        book_directory = os.path.join(version_directory_input, book_number)
         files = os.listdir(book_directory)
         for chapter in files:
             f_path = os.path.join(book_directory, chapter)
@@ -30,8 +32,15 @@ for version in versions:
                     parsed = html_parse(source)
                     h = parsed.find_all('h1')
                     book = list_first(h).text.strip()
+                    if not book_number in index:
+                        index[book_number] = {}
+                        index[book_number]["name"] = book
+                        index[book_number]["chapters"] = []
                     s = parsed.find_all('span', {"class":"chapread"})
                     chapter = list_first(s).text.strip()
+                    index[book_number]["chapters"].append(chapter)
+                    # Make sure the chapters are sorted by integer and not string
+                    index[book_number]["chapters"] = sorted(index[book_number]["chapters"], key=lambda e:int(e))
                     verses = str(parsed.find_all('p')[-2])
                     verses_list = verses.split('<br/>')
                     result = []
@@ -42,4 +51,8 @@ for version in versions:
                         tokens = s[1:] 
                         result.append({"book":book,"chapter":chapter,"verse":verse,"tokens":tokens})
                     json.dump(result, output, ensure_ascii=False, indent=4)
-    directory_for_each_if_numeric(version_directory, for_each)
+    directory_for_each_if_numeric(version_directory_input, for_each)
+    
+    output_path = os.path.join(version_directory_output, 'index.json')
+    with open(output_path, 'w', encoding='utf-8') as output:
+        json.dump(index, output, ensure_ascii=False, indent=4)
