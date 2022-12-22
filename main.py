1b02c5b23b6a1ecdd4c02344292b87f178340dae
JaredMathis <jared.mathis@native.consulting>
2022-12-21 23:27:48 -0500

    refacotr

--- main.py
+++ main.py
@@ -39,13 +39,13 @@ def http_get_save(url, path_save, decode_response=True):
             f.write(body)
 
 def vatican_download():
-    url_base = 'https://www.vatican.va/archive/ESL0506/'
-    url_root = url_base + "_INDEX.HTM"
-    download_if = lambda s: s.startswith('__')
+    links_download(
+        'https://www.vatican.va/archive/ESL0506/', 
+        "_INDEX.HTM", 
+        lambda s: s.startswith('__'))
 
-    links_download(url_base, url_root, download_if)
-
-def links_download(url_base, url_root, download_if):
+def links_download(url_base, url_index, download_if):
+    url_root = url_base + url_index
     b = BeautifulSoup(http_get_cached(url_root), features="html.parser")
     hrefs = ([c['href'] for c in b.find_all('a', href=True)])
     subs = [url_base + x for x in filter(download_if, hrefs)]
