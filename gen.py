b9873e8265be323c95970870911389bcf6c5c4bd
JaredMathis <jared.mathis@native.consulting>
2022-12-22 06:08:14 -0500

    refactor

--- gen.py
+++ gen.py
@@ -20,6 +20,9 @@ def file_html_parse(file_path):
         parsed = html_parse(source)
         return parsed
 
+def file_json_write(file_path, result):
+    with open(file_path, 'w', encoding='utf-8') as output:
+        json.dump(result, output, ensure_ascii=False, indent=4)
 
 for version in versions:
     index = {}
@@ -34,30 +37,29 @@ for version in versions:
             output_directory = os.path.join(directory_public, book_directory)
             dir_create_if_not_exists(output_directory)
             output_path = os.path.join(output_directory, str_ending_rename(chapter, '.htm', '') + ".json")
-            with open(output_path, 'w', encoding='utf-8') as output:
-                h = parsed.find_all('h1')
-                book = list_first(h).text.strip()
-                if not book_number in index:
-                    index[book_number] = {}
-                    index[book_number]["name"] = book
-                    index[book_number]["chapters"] = []
-                s = parsed.find_all('span', {"class":"chapread"})
-                chapter = list_first(s).text.strip()
-                index[book_number]["chapters"].append(chapter)
-                # Make sure the chapters are sorted by integer and not string
-                index[book_number]["chapters"] = sorted(index[book_number]["chapters"], key=lambda e:int(e))
-                verses = str(parsed.find_all('p')[-2])
-                verses_list = verses.split('<br/>')
-                result = []
-                for v in verses_list:
-                    t = html_parse(v).text.strip('\n')
-                    s = t.split(' ')
-                    verse = s[0]
-                    tokens = s[1:] 
-                    result.append({"book":book,"chapter":chapter,"verse":verse,"tokens":tokens})
-                json.dump(result, output, ensure_ascii=False, indent=4)
+            h = parsed.find_all('h1')
+            book = list_first(h).text.strip()
+            if not book_number in index:
+                index[book_number] = {}
+                index[book_number]["name"] = book
+                index[book_number]["chapters"] = []
+            s = parsed.find_all('span', {"class":"chapread"})
+            chapter = list_first(s).text.strip()
+            index[book_number]["chapters"].append(chapter)
+            # Make sure the chapters are sorted by integer and not string
+            index[book_number]["chapters"] = sorted(index[book_number]["chapters"], key=lambda e:int(e))
+            verses = str(parsed.find_all('p')[-2])
+            verses_list = verses.split('<br/>')
+            result = []
+            for v in verses_list:
+                t = html_parse(v).text.strip('\n')
+                s = t.split(' ')
+                verse = s[0]
+                tokens = s[1:] 
+                result.append({"book":book,"chapter":chapter,"verse":verse,"tokens":tokens})
+            file_json_write(output_path, result)
+
     directory_for_each_if_numeric(version_directory_input, for_each)
     
     output_path = os.path.join(version_directory_output, 'index.json')
-    with open(output_path, 'w', encoding='utf-8') as output:
-        json.dump(index, output, ensure_ascii=False, indent=4)
+    file_json_write(output_path, index)
